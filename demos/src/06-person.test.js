60898068f4707a9cd8a296300079b133ec618099
klich84 <klich84@gmail.com>
2024-07-15 17:15:51 -0500

    :man_student_medium_skin_tone: Metodologías

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -1,24 +1,33 @@
 const Person = require('./06-person')
 
+// AAA
+// Arrange  / Given
+// Act      / When
+// Assert   / Then
+
 describe('Test for Person class', () => {
   let person
-
+  // Arrange  / Given
   beforeEach(() => {
     person = new Person('Lorem', 1, 1)
   })
   test('should return down', () => {
+    // Arrange  / Given
     person.weight = 45
     person.height = 1.7
+    // Act      / When
     const imc = person.calcIMC()
-
+    // Assert   / Then
     expect(imc).toBe('down')
   })
 
   test('should return normal', () => {
+    // Arrange  / Given
     person.weight = 59
     person.height = 1.7
+    // Act      / When
     const imc = person.calcIMC()
-
+    // Assert   / Then
     expect(imc).toBe('normal')
   })
 })
