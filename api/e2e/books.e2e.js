d5b36c2a13dbd601a5e8d83e48ce2debf4ec5dcf
klich84 <klich84@gmail.com>
2024-07-24 08:11:44 -0500

    :man_student_medium_skin_tone: e2e

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,43 +1,63 @@
 /// En las pruebas de integración no debemos tocar las bases de datos
 
 const request = require('supertest')
-const mockGetAll = jest.fn()
+const { MongoClient } = require('mongodb')
 
 const createApp = require('../src/app')
-const { generateManyBooks } = require('../src/fakes/book.fake')
+const { config } = require('../src/config')
 
-// Cuando se llame esta libreria, quiero hacer remplazarla
-jest.mock('../src/lib/mongo.lib', () =>
-  jest.fn().mockImplementation(() => ({
-    getAll: mockGetAll,
-    create: () => {},
-  }))
-)
+const DB_NAME = config.dbName
+const MONGO_URI = config.dbUrl
 
 describe('Test for books', () => {
   let app = null
   let server = null
-  beforeAll(() => {
+  let database = null
+
+  beforeAll(async () => {
     app = createApp()
     server = app.listen(3001)
+
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    await client.connect()
+    database = client.db(DB_NAME)
   })
 
   afterAll(async () => {
     await server.close()
+
+    // Podemos agragar aca un borrado de la base de datos para qeu la base de datos quede limpia
+    await database.dropDatabase()
   })
 
   describe('test for [GET] /api/v1/books', () => {
     test('should return a list books', async () => {
       // Arange
-      const fakeBooks = generateManyBooks(3)
-      mockGetAll.mockResolvedValue(fakeBooks)
+      const seeData = await database.collection('books').insertMany([
+        {
+          name: 'Book 1',
+          year: 1998,
+          author: 'Carlos',
+        },
+        {
+          name: 'Book 2',
+          year: 1999,
+          author: 'Carlos',
+        },
+      ])
+
+      console.log('👽 ~ test ~ seeData:', seeData)
       // Act
       const response = await request(app).get('/api/v1/books') // El primer slash (/) es obligatorio
       // Assert
       console.log('👽 ~ test ~ body:', response.body)
 
       expect(response.statusCode).toEqual(200)
-      expect(response.body.length).toEqual(fakeBooks.length)
+      expect(response.body.length).toEqual(2)
     })
   })
 })
